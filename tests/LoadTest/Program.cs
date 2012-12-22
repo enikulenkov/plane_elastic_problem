85b6f16655c346b9bef747d9eeee3524ed06a359
Egor Nikulenkov <enikulenkov@gmail.com>
2012-12-22 15:52:12 +0400

    test for Load function of CommonData class

---
+++ tests/LoadTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Finite_Elements_method;
+
+namespace LoadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonData data = new CommonData();
+            foreach (string f in Directory.GetFiles("..\\..\\..\\..\\samples"))
+            {
+                data.Load(f);
+                Console.WriteLine("File {0} was read successfully", f);
+            }
+        }
+    }
+}
